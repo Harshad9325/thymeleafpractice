b4d207717af2a76bdb0801d42470d8d183ce3e97
autad <autad@DESKTOP-RQ59AHJ>
2023-03-21 14:29:49 +0530

    v.no 33,34,35

--- thymeleafPractice123/src/main/java/com/usd/controller/myController.java
+++ thymeleafPractice123/src/main/java/com/usd/controller/myController.java
@@ -1,5 +1,6 @@
 package com.usd.controller;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
@@ -11,26 +12,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class myController {
-	
-	@RequestMapping(value = "/about",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public String About(Model model) {
-		//puting data
-		model.addAttribute("name","harsh");
-		model.addAttribute("currentDate",new Date());
-		model.addAttribute("add","pune");
-		
+		// puting data
+		model.addAttribute("name", "harsh");
+		model.addAttribute("currentDate", new Date());
+		model.addAttribute("add", "pune");
+
 		System.out.println("this is handler");
-		
+
 		return "about";
 	}
-	
-	 // handling iteration
+
+	// handling iteration
 	@GetMapping("/example-loop")
-public String iterathandeller(Model m) {
-	//create a list
-		List<String> name = List.of("ankit","raj","karan","john");
-		m.addAttribute("names",name); 
-	return "iterat";
+	public String iterathandeller(Model m) {
+		// create a list
+		List<String> name = List.of("ankit", "raj", "karan", "john");
+		m.addAttribute("names", name);
+		return "iterat";
+	}
+
+	// handler for conditional statement
+	@GetMapping("/condition")
+	public String conditionalHandler(Model m) {
+
+		m.addAttribute("isActive", true);
+		m.addAttribute("gender", "M");
+
+		List<Integer> list = List.of();
+		m.addAttribute("Mylists", list);
+
+		System.out.println("condition handler exicuted");
+		return "condition";
+	}
+
+	// handler for including fragment
+	@GetMapping("/service")
+	public String serviceHandler(Model m) {
+		m.addAttribute("title", "first title");
+		m.addAttribute("substitle", LocalDateTime.now().toString());
+		return "service";
+	}
 }
-}
- 
